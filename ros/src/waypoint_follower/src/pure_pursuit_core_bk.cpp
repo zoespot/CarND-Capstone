e829740a14f6cd3588c9a16618d6cd4564b2aa9d
zoespot <zoe.liying@gmail.com>
2019-02-22 23:35:09 -0800

    version with Waypoint updater and DBW with wandering issue

--- ros/src/waypoint_follower/src/pure_pursuit_core_bk.cpp
+++ ros/src/waypoint_follower/src/pure_pursuit_core_bk.cpp
@@ -251,28 +251,23 @@ bool PurePursuit::verifyFollowing() const
 }
 geometry_msgs::Twist PurePursuit::calcTwist(double curvature, double cmd_velocity) const
 {
+  // verify whether vehicle is following the path
+  bool following_flag = verifyFollowing();
+  static double prev_angular_velocity = 0;
 
   geometry_msgs::Twist twist;
   twist.linear.x = cmd_velocity;
+  if (!following_flag)
+  {
+    //ROS_ERROR_STREAM("Not following");
+    twist.angular.z = current_velocity_.twist.linear.x * curvature;
+  }
+  else
+  {
+    twist.angular.z = prev_angular_velocity;
+  }
 
-  //TODO: always update even if it following the waypoints 
-  twist.angular.z = current_velocity_.twist.linear.x * curvature;
-
-  // static double prev_angular_velocity = 0;
-  // // verify whether vehicle is following the path
-  // bool following_flag = verifyFollowing();
-  // if (!following_flag)
-  // {
-  //   //ROS_ERROR_STREAM("Not following");
-  //   twist.angular.z = current_velocity_.twist.linear.x * curvature;
-  // }
-  // else
-  // {
-  //   twist.angular.z = prev_angular_velocity;
-  // }
-
-  // prev_angular_velocity = twist.angular.z;
-  
+  prev_angular_velocity = twist.angular.z;
   return twist;
 }
 
